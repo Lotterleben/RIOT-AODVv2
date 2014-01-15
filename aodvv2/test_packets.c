37d08c727fcdc5f8a792b2928921d69243bd8bf5
lotte steenbrink <lotte.steenbrink@fu-berlin.de>
2014-01-15 14:30:44 +0000

    +basic test rrep building (second try)

--- aodvv2/test_packets.c
+++ aodvv2/test_packets.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+#include "sender.h"
+#include "aodvv2_writer.h"
+#include "aodvv2_reader.h"
+
 #include "cunit/cunit.h"
 #include "common/netaddr.h"
-#include "aodvv2_writer.h"
 
 // 2nd try.
 static struct autobuf _hexbuf;
@@ -18,6 +21,44 @@ void test_rreq(void)
 
     START_TEST();
     writer_send_rreq(&origNode, &targNode);
+    END_TEST();
+}
+
+static void test_rrep(void)
+{
+    static struct netaddr address, origNode, targNode;
+    
+    netaddr_from_string(&address, "::12");
+    netaddr_from_string(&targNode, "::13");
+    netaddr_from_string(&origNode, "::14");
+
+    struct aodvv2_packet_data entry_1 = {
+        .hoplimit = AODVV2_MAX_HOPCOUNT,
+        .sender = address,
+        .metricType = AODVV2_DEFAULT_METRIC_TYPE,
+        .origNode = {
+            .addr = origNode,
+            .prefixlen = 128,
+            .metric = 12,
+            .seqNum = 13,
+        },
+        .targNode = {
+            .addr = targNode,
+            .prefixlen = 128,
+            .metric = 12,
+            .seqNum = 0,
+        },
+        .timestamp = NULL,
+    };
+
+    START_TEST();
+    printf("== Testing if RREP to someone else is forwarded: ==\n");
+    
+    writer_send_rrep(&entry_1);    
+    
+    //printf("== Testing if RREP to me is not forwarded: ==\n");
+    //netaddr_from_string(&entry_1.origNode.addr, MY_IP);
+    //send_rrep(&entry_1, &targNode);
     END_TEST();
 }
 
@@ -49,10 +90,16 @@ test_write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
 
 void test_packets_main(void)
 {
+    init_routingtable();
+    init_clienttable();
+    init_rreqtable();
+    init_seqNum();
+
     reader_init();
     writer_init(test_write_packet);
 
     BEGIN_TESTING(NULL);
     test_rreq();
+    test_rrep();
     FINISH_TESTING();
 }
