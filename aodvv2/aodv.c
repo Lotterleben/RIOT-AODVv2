7b668321c9fab70333962f5849cda4dffe8d7d9a
lotte steenbrink <lotte.steenbrink@fu-berlin.de>
2014-01-16 17:54:50 +0000

    +skeleton

--- aodvv2/aodv.c
+++ aodvv2/aodv.c
@@ -1,3 +1,5 @@
+#include <sixlowpan/ip.h>
+
 #include "include/aodvv2.h"
 #include "seqnum.h"
 #include "routing.h" 
@@ -8,9 +10,13 @@
 static void write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
     struct rfc5444_writer_target *iface __attribute__((unused)),
     void *buffer, size_t length);
+static ipv6_addr_t* get_next_hop(ipv6_addr_t* dest);
 
 void aodv_init(void)
 {
+    // TODO: init transceiver etc
+    
+    /* init ALL the things! \o, */
     init_seqNum();
     init_routingtable();
     init_clienttable();
@@ -18,12 +24,36 @@ void aodv_init(void)
 
     reader_init();
     writer_init(write_packet);
+
+    /* register aodv for routing */
+    ipv6_iface_set_routing_provider(get_next_hop);
 }
 
 
 static void write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
     struct rfc5444_writer_target *iface __attribute__((unused)),
-    void *buffer, size_t length) {
-
+    void *buffer, size_t length)
+{
     //TODO
+}
+
+static ipv6_addr_t* get_next_hop(ipv6_addr_t* dest)
+{
+    // TODO
+    return NULL;
+}
+
+static void aodv_sender_thread(void)
+{
+    // TODO
+}
+
+static void aodv_reciever_thread(void)
+{
+    // TODO
+}
+
+static void aodv_route_discovery_thread(void)
+{
+    // TODO
 }
