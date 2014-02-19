3025658b90f6d86774d64d0324f371bc72a624d9
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-02-19 11:03:56 +0000

    implemented 1st comdition for routing table entry expunging
       o  A route MUST be expunged if (Current_Time - Route.LastUsed) >=
          MAX_SEQNUM_LIFETIME.

--- aodvv2/constants.h
+++ aodvv2/constants.h
@@ -9,6 +9,7 @@
 #define AODVV2_DEFAULT_METRIC_TYPE 3
 #define AODVV2_ACTIVE_INTERVAL 5   // seconds
 #define AODVV2_MAX_IDLETIME 200    // seconds 
+#define MAX_SEQNUM_LIFETIME 300    // seconds
 #define AODVV2_RIOT_PREFIXLEN 128  // TODO: invent better name
 
 /* RFC5498 */
