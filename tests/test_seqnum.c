15dca25f953e34bd1a37f355c71930962a2b625b
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-03-01 16:53:34 +0000

    test foo update

--- tests/test_seqnum.c
+++ tests/test_seqnum.c
@@ -5,54 +5,54 @@
 
 static void test_seqnum_basic_functionality(void)
 {
-    uint16_t seqNum;
-    seqNum_init();
+    uint16_t seqnum;
+    seqnum_init();
 
     START_TEST();
-    seqNum = seqNum_get();
-    CHECK_TRUE(seqNum == 1, "SeqNum should be 1 instead of %d\n", seqNum);
-    seqNum_inc();
-    seqNum = seqNum_get();
-    CHECK_TRUE(seqNum == 2, "SeqNum should be 2 instead of %d\n", seqNum);    
+    seqnum = seqnum_get();
+    CHECK_TRUE(seqnum == 1, "seqnum should be 1 instead of %d\n", seqnum);
+    seqnum_inc();
+    seqnum = seqnum_get();
+    CHECK_TRUE(seqnum == 2, "seqnum should be 2 instead of %d\n", seqnum);    
     END_TEST();
 }
 
 /* Check if comparison works correctly around tipping point from 65535 to 1 */    
 static void test_seqnum_comparison_around_tipping_point(void)
 {
-    uint16_t seqNum;
-    int seqNum_cmp;
-    seqNum_init();
+    uint16_t seqnum;
+    int seqnum_c;
+    seqnum_init();
 
-    while (seqNum_get() < 65525){
-        seqNum_inc();
+    while (seqnum_get() < 65525){
+        seqnum_inc();
     }
 
     START_TEST();
-    seqNum = seqNum_get();
-    CHECK_TRUE(seqNum_get() == 65525, "SeqNum should be 65525 instead of %d\n", seqNum);
-    seqNum = seqNum_get();
-    seqNum_cmp = seqnum_cmp(seqNum, 1);
-    CHECK_TRUE(seqNum_cmp == -1, "SeqNum should be smaller than 1 instead of cmp_seqnum result %i\n", seqnum_cmp);    
+    seqnum = seqnum_get();
+    CHECK_TRUE(seqnum_get() == 65525, "seqnum should be 65525 instead of %d\n", seqnum);
+    seqnum = seqnum_get();
+    seqnum_c = seqnum_cmp(seqnum, 1);
+    CHECK_TRUE(seqnum_c == -1, "seqnum should be smaller than 1 instead of cmp_seqnum result %i\n", seqnum_c);    
     END_TEST();
 }
 
-/* Check if SeqNum tips over from 65535 to 1 correctly */
+/* Check if seqnum tips over from 65535 to 1 correctly */
 static void test_seqnum_tipping()
 {
-    uint16_t seqNum;
-    seqNum_init();
+    uint16_t seqnum;
+    seqnum_init();
 
-    while (seqNum_get() < 65535){
-        seqNum_inc();
+    while (seqnum_get() < 65535){
+        seqnum_inc();
     }
 
     START_TEST();
-    seqNum = seqNum_get();
-    CHECK_TRUE(seqNum == 65535, "SeqNum should be 655235 instead of %d\n", seqNum);    
-    seqNum_inc();
-    seqNum = seqNum_get();
-    CHECK_TRUE(seqNum == 1, "SeqNum should be 1 instead of %d\n", seqNum);    
+    seqnum = seqnum_get();
+    CHECK_TRUE(seqnum == 65535, "seqnum should be 655235 instead of %d\n", seqnum);    
+    seqnum_inc();
+    seqnum = seqnum_get();
+    CHECK_TRUE(seqnum == 1, "seqnum should be 1 instead of %d\n", seqnum);    
     END_TEST();
 }
 
