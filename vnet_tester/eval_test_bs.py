08ddac35b31e0b2aec9d5d329c54336cde9430a9
Lotte Steenbrink <lotte@zombietetris.de>
2014-04-22 23:35:17 +0200

    read src and dst ip

--- vnet_tester/eval_test_bs.py
+++ vnet_tester/eval_test_bs.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup, NavigableString
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pretty_plots as pp
 
 RFC5444_MSGTYPE_RREQ = "10"
 RFC5444_MSGTYPE_RREP = "11"
@@ -17,10 +18,12 @@ RFC5444_MSGTLV_TARGSEQNUM = "1"
 RFC5444_MSGTLV_UNREACHABLE_NODE_SEQNUM = "2"
 RFC5444_MSGTLV_METRIC = "3"
 
-working_dir = "./dumps/"
-packets = [] # store ALL the packets!
+DISCOVERY_ATTEMPTS_MAX = 3
 
-def store_pkt(packetbb):
+working_dir = "./dumps/"
+packets = []
+
+def store_pktbb(packetbb):
     pkt = {}
     orignode = {}
     targnode = {}
@@ -80,8 +83,24 @@ def store_pkt(packetbb):
 
         pkt["unreachable_nodes"] = unreachable_nodes
 
-    print "\t", pkt
-    packets.append(pkt)
+    #print "\t", pkt
+    return pkt
+
+def store_data(pkt):
+    ip_header = pkt.find(attrs = {"name":"ipv6"})
+    src_addr = ip_header.find(attrs = {"name":"ipv6.src"})["show"]
+    dst_addr = ip_header.find(attrs = {"name":"ipv6.dst"})["show"]
+    data = ""
+
+    #data = pkt.find(attrs = {"name":"ipv6.dst"})["show"]
+    packetbb = pkt.find(attrs = {"name":"packetbb"})
+    if (packetbb):
+        #print "i can haz packetbb", type(packetbb), type(ip_header)
+        data = store_pktbb(packetbb)
+
+    foo = {"src": src_addr, "dst": dst_addr, "data": data}
+
+    print foo
 
 
 def pcap_to_xml(pcap_file_str):
@@ -98,7 +117,7 @@ def pcap_to_xml(pcap_file_str):
     # make pcap python-readable by converting it to xml, store in file
     if (os.path.isfile(xml_file_location)):
         os.remove(xml_file_location)
-    os.system("tshark -r %s -V -Y packetbb -T pdml >> %s" %(pcap_file_str, xml_file_location))
+    os.system("tshark -r %s -V -Y ipv6 -T pdml >> %s" %(pcap_file_str, xml_file_location))
 
     return xml_file_location
 
@@ -109,9 +128,20 @@ def handle_capture(xml_file_location):
     xml_file = open(xml_file_location, "r")
     soup = BeautifulSoup(xml_file, "xml")
 
+    ''' commented out for debugging
     packetbb_pkts = soup.find_all(attrs = {"name":"packetbb"})
-    for packetbb in packetbb_pkts:
-        store_pkt(packetbb)
+    for pkt in packetbb_pkts:
+        store_pktbb(pkt)
+
+    data_pkts = soup.find_all(attrs = {"name":"ipv6"})
+    print data_pkts
+    for pkt in data_pkts:
+        store_data(pkt)
+    '''
+
+    packets = soup.find_all("packet")
+    for pkt in packets:
+        store_data(pkt)
 
 
 def handle_logfile(log_file_location):
@@ -119,23 +149,68 @@ def handle_logfile(log_file_location):
         print "Couldn't find log file. Aborting."
         return
 
-    (success, fail) = get_successful_transmissions(log_file_location)
-    print success, fail
+        # todo update me
+    results = count_successes(log_file_location)
+    print results
 
-    plot_bars(("successful", "failed"), (success, fail))
+    successes = (results["discoveries"]["success"], results["transmissions"]["success"])
+    failures = (results["discoveries"]["fail"], results["transmissions"]["fail"])
 
-def get_successful_transmissions(log_file_location):
+    # TODO discoveries within timeout into plot
+    pp.plot_bars(("successful", "failed"),("Route Discoveries", "Transmissions"), successes, failures)
+
+
+def count_successes(log_file_location):
+    print "counting successful route discoveries and transmissions..."
     logfile = open(log_file_location)
-    success = 0
-    fail = 0
+    rreqs_sent = {}
+    rreqs_total = 0
+
+    discoveries = {"success" : 0, "fail" : 0}
+    discoveries_within_timeout = 0
+    transmissions = {"success" : 0, "fail" : 0}
 
     for line in logfile:
-        if ("[demo]  Success sending" in line):
-            success += 1
-        if ("[demo]  Error sending" in line):
-            fail += 1
+        # TODO: sowas (aber mit ip des senders..) partsen [demo]   sending packet of %i bytes towards %s...\n"
 
-    return (success, fail)
+        # look for successful transmission
+        if ("[demo]  Success sending" in line):
+            transmissions["success"] += 1
+        elif ("[demo]  Error sending" in line):
+            transmissions["fail"] += 1
+
+        # look for successful route discovery
+        # (here's to hoping I'll never change that debug output...)
+        elif ("[aodvv2] originating RREQ" in line):
+            # TODO ip rausfiltern
+            ip = line.split(" ")[4].strip()[:-1]
+            print "originating ip: ", ip #, line
+            try:
+                # found (possibly redundant) RREQ
+                if (rreqs_sent[ip] < DISCOVERY_ATTEMPTS_MAX):
+                    rreqs_sent[ip] += 1
+                # rreq has been sent without success last time; reset counter
+                else:
+                    rreqs_sent[ip] = 1
+                    discoveries["fail"] += 1
+            except:
+                # this is the first rreq to ip
+                rreqs_sent[ip] = 0
+
+            print rreqs_sent
+
+        # if rrep found:
+        elif ("This is my RREP." in line):
+            ip = line.split(" ")[1].strip()[:-1]
+            print "found ip: ", ip #, line
+            discoveries["success"] += 1
+            if (rreqs_sent[ip] < 3 ):
+                print "OMG!!! Success within timeout!"
+                discoveries_within_timeout += 1
+            rreqs_sent[ip] = 0
+
+    return {"discoveries" : discoveries, "transmissions" : transmissions, "discoveries_within_timeout" : discoveries_within_timeout}
+
 
 def plot_bars(labels, values):
 
@@ -166,7 +241,7 @@ def main():
         print "evaluating log..."
         handle_logfile(args.log)
 
-    #print packets
+    #print pktbb_packets
 
 if __name__ == "__main__":
     main()
