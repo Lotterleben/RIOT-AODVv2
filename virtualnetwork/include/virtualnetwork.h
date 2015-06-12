7a25b3e8be65ee828655d842dd1ea2dfe9280416
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2015-06-11 22:40:13 -0700

    + -DTEST_SETUP

--- virtualnetwork/include/virtualnetwork.h
+++ virtualnetwork/include/virtualnetwork.h
@@ -22,3 +22,20 @@ int virtualnetwork_sendto(int s, const void *buf, uint32_t len, int flags,
  * @param   next_hop    function that returns the next hop to reach dest
  */
 void virtualnetwork_set_routing_provider(ipv6_addr_t *(*next_hop)(ipv6_addr_t *dest));
+
+/**
+ * Substitute for socket_base_recvfrom().
+ * Some of the fields are ignored, but have been left in for easier portability.
+ *
+ * @param[in] s         The ID of the socket to receive from.
+ * @param[in] buf       Buffer to store received data in.
+ * @param[in] len       Length of buffer.
+ * @param[in] flags     Flags for possible later implementations (currently
+ *                      unused).
+ * @param[in] from      IPv6 Address of the data's sender.
+ * @param[in] fromlen   Length of address in *from* in byte (always 16).
+ *
+ * @return Number of received bytes, -1 on error.
+ */
+int32_t socket_base_recvfrom(int s, void *buf, uint32_t len, int flags,
+                                sockaddr6_t *from, socklen_t *fromlen);
